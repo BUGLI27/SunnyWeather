1aa67c086919e6f4300c4c6845a592a7a137b17c
BUGLI <2766071035@qq.com>
2022-02-06 19:47:12 +0800

    Weather页面

---
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -0,0 +1,98 @@
+package com.example.sunnyweather.ui.weather
+
+import android.icu.text.SimpleDateFormat
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.sunnyweather.R
+import com.example.sunnyweather.logic.model.Weather
+import com.example.sunnyweather.logic.model.getSky
+import com.example.sunnyweather.ui.place.WeatherViewModel
+import java.util.*
+
+class WeatherActivity : AppCompatActivity() {
+
+    val viewModel by lazy { ViewModelProvider(this).get(WeatherViewModel::class.java) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+        if (viewModel.locationLng.isEmpty()) {
+            viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
+        }
+        if (viewModel.locationLat.isEmpty()) {
+            viewModel.locationLat = intent.getStringExtra("location_lat") ?: ""
+        }
+        if (viewModel.placeName.isEmpty()) {
+            viewModel.placeName = intent.getStringExtra("place_name") ?: ""
+        }
+        viewModel.weatherLiveData.observe(this, Observer { result ->
+            val weather = result.getOrNull()
+            if (weather != null) {
+                showWeatherInfo(weather)
+            } else {
+                Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun showWeatherInfo(weather: Weather) {
+        val placeName = findViewById<TextView>(R.id.placeName)
+        placeName.text = viewModel.placeName
+        val realtime = weather.realtime
+        val daily = weather.daily
+        // 填充now.xml布局中的数据
+        val currentTemp = findViewById<TextView>(R.id.currentTemp)
+        val currentSky = findViewById<TextView>(R.id.currentSky)
+        val currentAQI = findViewById<TextView>(R.id.currentAQI)
+        val nowLayout = findViewById<RelativeLayout>(R.id.nowLayout)
+        val currentTempText = "${realtime.temperature.toInt()} ℃"
+        currentTemp.text = currentTempText
+        currentSky.text = getSky(realtime.skycon).info
+        val currentPM25Text = "空气指数 ${realtime.airQuality.api.chn.toInt()}"
+        currentAQI.text = currentPM25Text
+        nowLayout.setBackgroundResource(getSky(realtime.skycon).bg)
+        // 填充forecast.xml布局中的数据
+        val forecastLayout = findViewById<LinearLayout>(R.id.forecastLayout)
+        forecastLayout.removeAllViews()
+        val days = daily.skycon.size
+        for (i in 0 until days) {
+            val skycon = daily.skycon[i]
+            val temperature = daily.temperature[i]
+            val view = LayoutInflater.from(this).inflate(R.layout.forecast_item, forecastLayout, false)
+            val dateInfo = view.findViewById(R.id.dateInfo) as TextView
+            val skyIcon = view.findViewById(R.id.skyIcon) as ImageView
+            val skyInfo = view.findViewById(R.id.skyInfo) as TextView
+            val temperatureInfo = view.findViewById(R.id.temperatureInfo) as TextView
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            dateInfo.text = simpleDateFormat.format(skycon.date)
+            val sky = getSky(skycon.value)
+            skyIcon.setImageResource(sky.icon)
+            val tempText = "${temperature.min.toInt()} ~ ${temperature.max.toInt()} ℃"
+            temperatureInfo.text = tempText
+            forecastLayout.addView(view)
+        }
+        // 填充life_index.xml中的数据
+        val lifeIndex = daily.lifeIndex
+        val coldRiskText = findViewById<TextView>(R.id.coldRiskText)
+        val dressingText = findViewById<TextView>(R.id.dressingText)
+        val ultravioletText = findViewById<TextView>(R.id.ultravioletText)
+        val carWashingText = findViewById<TextView>(R.id.carWashingText)
+        coldRiskText.text = lifeIndex.coldRisk[0].desc
+        dressingText.text = lifeIndex.dressing[0].desc
+        ultravioletText.text = lifeIndex.ultraviolet[0].desc
+        carWashingText.text = lifeIndex.carWashing[0].desc
+        // 显示weatherLayout
+        val weatherLayout = findViewById<ScrollView>(R.id.weatherLayout)
+        weatherLayout.visibility = View.VISIBLE
+    }
+}
